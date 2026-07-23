c6e650177c664e19749f7cd81c7a9147c2d6bcba
Prabh231 <200651351@student.georgianc.on.ca>
2026-07-23 09:39:40 -0400

    added server information

---
+++ classes/Database.php
@@ -0,0 +1,13 @@
+<?php
+
+class Database {
+
+    private string $host = '172.31.22.43';
+    private string $db_name = 'Prabhjot200651351';
+    private string $username = 'Prabhjot200651351';
+    private string $password = 'zsNffVJ0B-';
+
+    private ?PDO $connection = null;
+    
+}
+?>
