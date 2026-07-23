964513909d592532c16cc7b4618a8db8498e228d
Prabh231 <200651351@student.georgianc.on.ca>
2026-07-23 10:39:39 -0400

    corrected name as i did in process_order.php

--- index.php
+++ index.php
@@ -4,21 +4,21 @@
     <h2>Place Your Pizza Order</h2>
     <form action="process_order.php" method="POST">
         <label>Full Name: </label>
-        <input type="text" name="name" required>
+        <input type="text" name="customer_name" required>
         <label>Email: </label>
         <input type="email" name="email"required>
         <label>Phone: </label>
         <input type="tel" name="phone" required>
         <label>Pizza Size:</label>
-        <select name="size" required>
+        <select name="pizza_size" required>
             <option value="">Select Size</option>
             <option>Small</option>
             <option>Medium</option>
             <option>Large</option>
         </select>
         <label>Pizza Type:</label>
-        <input type="radio" name="type" value="Veggie" required>Veggie
-        <input type="radio" name="type" value="Pepperoni">Pepperoni
+        <input type="radio" name="pizza_type" value="Veggie" required>Veggie
+        <input type="radio" name="pizza_type" value="Pepperoni">Pepperoni
         <label>Toppings:</label>
         <input type="checkbox" name="toppings[]" value="Cheese">Extra Cheese
         <input type="checkbox" name="toppings[]" value="Mushroom">Mushroom
@@ -26,7 +26,7 @@
         <label>Quantity:</label>
         <input type="number" name="quantity" min="1"required>
         <label>Delivery Address:</label>
-        <textarea name="address" required></textarea>
+        <textarea name="delivery_addressaddress" required></textarea>
         <button type="submit">Place Order</button>
     </form>
     </section>
